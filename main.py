6f638dcc99a4c39f890f82749147b43ce42330be
vamsi shyam <v.chaparala@gmail.com>
2025-07-07 21:31:59 +0530

    Update file

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
+#function
+def display_range():
+    for i in range(10):
+        print(i)
 
+
+display_range()
+
+#try except
+try:
+    f = open("sample.txt","w")
+    f.write("Hello World")
+except FileNotFoundError:
+    print("File not found")
+finally:
+    f.close()
+
+#class
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def display(self):
+        print(self.name, self.age)
+
+person1 = Person("Vamsi", 34)
+person2 = Person("Shyam", 26)
+
+person1.display()
+person2.display()
+
+#entry point
 if __name__ == '__main__':
     print('PyCharm')
-
 
