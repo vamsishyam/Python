c0b936a69b62bb0ab8ebd2d96e04a3fc2e147473
vamsi shyam <v.chaparala@gmail.com>
2025-07-07 22:05:25 +0530

    Updated with more stuff

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+import threading as t
+from time import sleep
+from calc import add
+
+print(add(26,12))
+
 #function
-def display_range():
-    for i in range(10):
+def display_range(*lst):
+    """ display a list received """
+    for i in lst:
         print(i)
+        sleep(1)
 
 
-display_range()
+thread = t.Thread(target=display_range, args=(['a','b','c']))
+thread.start()
 
 #try except
 try:
@@ -28,6 +37,8 @@ person2 = Person("Shyam", 26)
 
 person1.display()
 person2.display()
+
+thread.join()
 
 #entry point
 if __name__ == '__main__':
